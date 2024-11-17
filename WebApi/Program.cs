9327d4e3a528fe8f39e585fac3f6f1ff8f20d9a2
Sadettin49 <sadettinyilmaz4933@gmail.com>
2024-11-17 15:48:40 +0300

    17.11

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
-builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureLoggerService(); //yorum
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureActionFilters();
 builder.Services.ConfigureCors();
